8c3e7e6ee93ff98c2400dfccdaff88193bec6f51
Austin Jones` <austinhjones3@gmail.com>
2021-08-19 15:17:41 -0500

    make list view desktop responsive

--- src/pages/ListReviews.tsx
+++ src/pages/ListReviews.tsx
@@ -2,11 +2,12 @@ import Context from "../context";
 import { useContext } from "react";
 import { IContext, IReview } from "../types";
 import ReviewItem from "../components/ReviewItem";
+import "../styles/ListReviews.css";
 
 const ListReviews = (): JSX.Element => {
   const context: IContext = useContext(Context);
   return (
-    <div>
+    <div id="list-container">
       {context.state.data?.map((ele: IReview, idx: number) => (
         <div className="review-item" key={idx}>
           <ReviewItem review={ele} />
